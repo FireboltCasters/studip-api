7b4c458854e63eef2f92fb351ab46f848e13699f
Nils <nilsbaumgartner1994@gmail.com>
2021-09-12 18:48:48 +0200

    Added Fakebackend to export

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,11 @@ import UrlHelper from './UrlHelper';
 import Perm from './models/Perm';
 import User from './models/User';
 import ScheduleEvent from './models/ScheduleEvent';
+import FakeBackend from "./ignoreCoverage/FakeBackend";
 
 export {Connector};
 export {UrlHelper};
 export {Perm};
 export {User};
 export {ScheduleEvent};
+export {FakeBackend};
