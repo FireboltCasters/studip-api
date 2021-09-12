cb4c2b5d7be87d9afc6bfbf1c8ec000d3a89514f
Nils <nilsbaumgartner1994@gmail.com>
2021-09-12 15:07:05 +0200

    Fixed Deprecated Buffer Method

--- src/Base64Helper.ts
+++ src/Base64Helper.ts
@@ -1,12 +1,12 @@
 export default class Base64Helper {
 
   static toBase64(string: string){
-    let buff = new Buffer(string);
+    let buff = Buffer.from(string);
     return buff.toString('base64');
   }
 
   static fromBase64(string: string){
-    let buff = new Buffer(string, 'base64');
+    let buff = Buffer.from(string, 'base64');
     return buff.toString('ascii');
   }
 
